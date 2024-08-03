b82b58c3089807fcc4a032fad6529adcce05e483
Raghav Kumar <jharaghav330@gmail.com>
2024-08-03 15:20:58 +0530

    add some routes in user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
+import { accountDetailsUpdate, changePassword, currentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAvatar, updateCoverImage } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
@@ -19,5 +19,14 @@ router.route('/login').post(loginUser)
 // secure routes
 router.route('/logout').post(verifyToken, logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
+router.route('/change-password').post(verifyToken, changePassword)
+router.route('/current-user').get(verifyToken, currentUser)
+router.route('/account-update').patch(verifyToken, accountDetailsUpdate)
+
+router.route('/avatar').patch(verifyToken, upload.single('avatar'), updateAvatar)
+router.route('/cover-image').patch(verifyToken, upload.single('coverImage'), updateCoverImage)
+
+router.route('/channel').get(verifyToken, getUserChannelProfile)
+router.route('/watch-history').post(verifyToken, getWatchHistory)
 
 export default router
